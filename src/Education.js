829297b667360455c45c30233f4f06434a9effe7
Nicole0320 <difan0320@126.com>
2017-05-16 21:13:59 +0800

    添加样式

--- src/Education.js
+++ src/Education.js
@@ -9,7 +9,7 @@ export default class Education extends Component{
         return(
             <div className="MainPage Education">
                 <h1 className="title">教育经历</h1>
-                <h1>西安电子科技大学</h1>
+                <h2>西安电子科技大学</h2>
                 <p>软件工程 | 本科 | 2015年毕业</p>
             </div>
         );
