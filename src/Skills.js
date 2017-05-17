4c9dc9c22bf751df21ad29c9df944e43bb2c0060
Nicole0320 <difan0320@126.com>
2017-05-17 18:19:47 +0800

    add Skills page

--- src/Skills.js
+++ src/Skills.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './Skills.css';
 
 export default class Skills extends Component{
     constructor(props){
@@ -7,7 +8,20 @@ export default class Skills extends Component{
 
     render(){
         return(
-            <div>专业技能</div>
+            <div className="MainPage Skills">
+                <h1 className="title">专业技能</h1>
+                <ul className="tags">
+                    <li>JavaScript</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Ajax</li>
+                    <li>React</li>
+                    <li>jQuery</li>
+                    <li>算法</li>
+                    <li>计算机操作系统基础</li>
+                    <li>数据结构</li>
+                </ul>
+            </div>
         );
     }
 }
