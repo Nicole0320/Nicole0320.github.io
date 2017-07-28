a9bac1c726d6dd86ab5dbc8f5c0192d79d9f2519
difan0320@126.com <difan0320@126.com>
2017-07-28 23:55:02 +0800

    删除调试代码

--- src/Skills.js
+++ src/Skills.js
@@ -17,7 +17,7 @@ export default class Skills extends Component{
                     <li><div>&rArr;</div><p> 熟悉<span className="highlight-1">React框架</span>，理解生命周期、异步更新、虚拟DOM 等重要概念，可基于 React 实现web应用，并有独立完成的项目实践（todoList，以及本简历展示应用）。</p></li>
                     <li><div>&rArr;</div><p> 了解<span className="highlight-1">AJAX</span>的原理及应用，了解 JSONP、CORS 等<span className="highlight-2">跨域</span>解决方案，能与后端程序员配合实现动态页面。</p></li>
                     <li><div>&rArr;</div><p> 熟悉<span className="highlight-1">HTTP 协议</span>以及浏览器渲染机制。</p></li>
-                    <li><div>&rArr;</div><p> 了解<span className="highlight-2">npm&amp;Git</span>,，有相关使用经验，能使用Git工具对代码进行版本管理。</p></li>
+                    <li><div>&rArr;</div><p> 了解<span className="highlight-2">npm&amp;Git</span>，有相关使用经验，能使用Git工具对代码进行版本管理。</p></li>
                     <li><div>&rArr;</div><p> 了解<span className="highlight-2">算法</span>、数据结构等基本知识，可以解决开发过程中常见的逻辑问题。</p></li>
                     <li><div>&rArr;</div><p> 有 C/C++/C# 等C系语言使用经验，了解计算机系统知识，可以与后端开发人员无障碍沟通。</p></li>
                     <li><div>&rArr;</div><p> 追求优雅规范的代码风格。</p></li>
