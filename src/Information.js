1ce98685fc35e9ac1c91b773388198e35fcd791c
difan0320@126.com <difan0320@126.com>
2017-08-02 18:16:17 +0800

    更改位置信息

--- src/Information.js
+++ src/Information.js
@@ -13,8 +13,8 @@ export default class Information extends Component{
                 <h3>应聘职位：前端开发工程师</h3>
                 <p>只因兴趣，所以选择；只因热爱，所以为每一像素执着</p>
                 <ul className="contact">
-                    <li><i className="iconfont icon-address"></i> | 当前坐标：西安</li>
-                    {/*<li><i className="iconfont icon-mudedi"></i> | 目标城市：杭州、北京、西安</li>*/}
+                    {/* <li><i className="iconfont icon-address"></i> | 当前坐标：西安</li> */}
+                    <li><i className="iconfont icon-address"></i> | 目标城市：杭州、北京、西安、深圳</li>
                     <li><i className="iconfont icon-tele"></i> | 联系电话：18740456705</li>
                     <li><i className="iconfont icon-youxiang"></i> | 电子邮箱：difan0320@126.com</li>
                     <li><i className="iconfont icon-github"></i> | <a href="https://github.com/Nicole0320" target="_blank">GitHub：点击进入</a></li>
